c4c931e5d487cd0fe381aad3a514a2dc8631b258
Gerardo Gonzalez <ggonza14@ut.utm.edu>
2024-01-17 19:41:45 -0600

    second commit

--- main.cpp
+++ main.cpp
@@ -3,9 +3,21 @@
 
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
+int sum (int n){
+  int add;
+  int i = 1;
+  while(i < n){
+    add = i + n;
+    i++;
+  }
+  return add;
+}
 
-  
+int main() {
+  cout << "Hello World!" << endl;
+  int n;
+  cout << "Give me a number, any number: ";
+  cin >> n;
+  cout << "Your new number is: " << sum(n) << endl;
   return 0;
 }
